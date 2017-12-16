cde5ebffd2a42d84751f6e38f326109898bf56cf
cerico <garethrobertlee@gmail.com>
2017-12-16 23:25:56 +0000

    include server in build

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,5 @@
 source bin/config.env
 rsync --rsh=ssh -av server.js "root@$server:/var/www/html/$appname"
 rsync --rsh=ssh -av dist "root@$server:/var/www/html/$appname"
+rsync --rsh=ssh -av server "root@$server:/var/www/html/$appname"
 ssh root@$server pm2 restart $appname
